a06fac52dfb70b24683723d7703b978298deb584
Uttam Sharma <57016707+uttam-sharma786@users.noreply.github.com>
2021-11-18 13:02:42 +0530

    2

---
+++ Publication.php
@@ -0,0 +1,113 @@
+<?php
+require_once("function.php");
+site_header();
+
+?>
+
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rohan | Master's,Bioinformatics</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Rohan ">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    
+    
+    <!--- link --->
+<!--
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css"> 
+    <link rel="canonical" href="#"> 
+    <!-- end -->
+
+    
+    
+     <!-- Custom CSS -->
+<!--
+     <link rel="stylesheet" href="style.css/style.css"> 
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    
+</head>
+-->
+
+
+
+<!--
+<body>
+	<!-- This is a navbar start -->
+	
+<!--
+	<div class="wrap-1">
+		<header class="container">
+            <div class="row">
+				<div class="col-3 col-t-5 col-m-6 col-p-9">
+					<h1><a href="Rohan.php">Rohan Ghosh</a></h1>
+				</div>
+			<div>              
+                  <nav class="col-9 col-t-9">
+                        <ul>
+                            <li><a href="Rohan.php">About</a></li>
+                            <li><a href="Publication.php">Publication</a></li>
+                            <li><a href="Academics.php">Academics</a></li>
+                            <li><a href="RWorkshop.php">Associates</a></li>
+                        </ul>
+                    </nav>
+                    <div class="clear"></div>
+               </div>
+            </div>
+    </header>
+</div>
+-->
+
+<!-- This is a navbar start end -->
+	
+<div class="Publications">
+<div class="col-12 col-t-12 col-m-12 ">
+<h2 style="text-align: center;">Publications</h2>
+<table>                                         
+<tr>
+<table>
+<ol>
+<a href=""></a>
+<h3>2021</h3>
+</tr>
+</table>
+</div>
+<hr><h2 style="text-align: center;">Book's Chapter</h2>
+<table>
+<li><b>"DNA Barcoding: The Conspicuous Scan" written by Somenath Dutta & Rohan Ghosh.<p><a href="#">Compendium of "RESEARCH INSIGHTS OF LIFE SCIENCE STUDENTS (RILSS)" Volume-3 by JPS Publication </a></li></p>
+</div>
+</tr>
+</table>
+
+<!--- Social media --->
+ 
+<div class="wrap-4 col-12 col-m-12 col-p-12">
+	<div class="container5">
+		<div class="row">
+			<div class="social">
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.linkedin.com/in/rohan-ghosh-8a53a0155" target="_blank"><i class="fab fa-linkedin"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.researchgate.net/profile/Rohan-Ghosh-6" target="_blank"><i class="fab fa-researchgate"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://twitter.com/RohanG_26" target="_blank"><i class="fab fa-twitter"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.facebook.com/rohan.ghosh.520900" target="_blank"><i class="fab fa-facebook"></i></a></div>
+            </div>
+        </div>
+	</div>
+</div>
+    
+<!--
+</body>
+
+-->
+
+<!-- social media end -->
+<!--
+</html>
+-->
+
