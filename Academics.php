1fb6c9abbc3a6fc5e6b8ebc950214574e328bed3
Uttam Sharma <57016707+uttam-sharma786@users.noreply.github.com>
2021-11-18 13:03:45 +0530

    4

---
+++ Academics.php
@@ -0,0 +1,145 @@
+<?php
+require_once("function.php");
+site_header();
+
+?>
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rohan |  Master's,Bioinformatics</title>
+    <meta charset="UTF-8">
+    <link rel="canonical" href="#">
+    <meta name="description" content="Rohan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    
+    <!--- link --->
+<!--
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">  
+     <!-- end -->
+     
+     
+     
+     <!-- Custom CSS -->
+<!--
+     <link rel="stylesheet" href="style.css/style.css"> 
+     <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+		
+</head>
+-->
+
+
+<!--
+
+<body>
+	<div class="wrap-1">
+		<header class="container">
+            <div class="row">
+				<div class="col-3 col-t-5 col-m-6 col-p-9">
+					<h1><a href="Rohan.html">Rohan Ghosh</a></h1>
+				</div>
+				<div>
+                 
+                   
+                    <nav class="col-9 col-t-9">
+                        <ul>
+                            <li><a href="Rohan.html">About</a></li>
+                            <li><a href="Publication.html">Publication</a></li>
+                            <li><a href="Academics.html">Academics</a></li>
+                            <li><a href="RWorkshop.html">Associates</a></li>
+                        </ul>
+                    </nav>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    </div>
+-->
+
+<div class="experience">
+<div class="col-12 col-t-12 col-m-12 ">
+<h2 style="text-align: center;">Academics</h2>
+<table>                                         
+<tr>
+<td><h3>2022:</h3></td>
+<td><b>Master in Science (M.Sc.)</b>, Bioinformatics
+<br>
+<i>Centre for Bioinformatics, Pondicherry Central University, Puducherry, India</i></td>
+</tr>
+<tr><tr>
+<td><h3>2020:</h3></td>
+
+<td><b>Bachelors in Science (B.Sc.)</b>, Microbiology
+<br>
+<i> West Bengal State University, India</i></td>
+</tr>
+</table>
+</div>
+<hr>
+<h2 style="text-align: center;">Courses</h2>
+<table>   
+<tr>
+<td><h3>2021:</h3></td>
+<td><b>Computed Assisted Drug Design </b>(CADD)
+<br>
+<i>Birla Institute of Scientific Research (BISR), Design Innovation Centre (DIC), Rajasthan,India</i></td>                                      
+<tr>
+</tr>
+<tr>
+<td><h3>2021:</h3></td>
+<td><b>Summer School on Machine Learning in Bioinformatics 2021</b> (Virtual)
+<br>
+<i>Conducted by HSE University, Moscow, Russia</i></td>                                      
+<tr>
+</tr>
+</table>
+</div>
+<hr>
+<div class="col-12 col-t-12 col-m-12 ">
+<h2 style="text-align: center;">Scientific Projects</h2>
+<table>
+<tr>
+<td><h4>2021-Present:</h4></td>
+<td><b>In-silico study of anti-bacterial phytochemicals for development of drugs aginst Methicillin-resistant Staphylococcus aureus. </b>
+<br>
+<i>Supervisor:Dr. M. Krishnamohan, Scientist of Birla Institute Of Scientific Research (BISR) Rajasthan, India</i></td>
+</tr>
+</tr>
+</table>
+</div>
+<div class="clear"></div>
+ </div>
+</div>
+ </div>
+</div>
+
+<!--- Social media --->
+ 
+<div class="wrap-4 col-12 col-m-12 col-p-12">
+	<div class="container5">
+		<div class="row">
+			<div class="social">
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.linkedin.com/in/rohan-ghosh-8a53a0155" target="_blank"><i class="fab fa-linkedin"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.researchgate.net/profile/Rohan-Ghosh-6" target="_blank"><i class="fab fa-researchgate"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://twitter.com/RohanG_26" target="_blank"><i class="fab fa-twitter"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.facebook.com/rohan.ghosh.520900" target="_blank"><i class="fab fa-facebook"></i></a></div>
+            </div>
+        </div>
+	</div>
+</div>
+    
+
+
+
+<!-- social media end -->
+<!--
+</body>
+</html>
+-->
