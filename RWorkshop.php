36b90fcb1d8a9f9a338d58c9971504ba48f061cb
Uttam Sharma <57016707+uttam-sharma786@users.noreply.github.com>
2021-11-18 13:03:13 +0530

    3

---
+++ RWorkshop.php
@@ -0,0 +1,122 @@
+<?php
+require_once("function.php");
+site_header();
+
+?>
+
+
+
+
+<!--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Rohan | Master's,Bioinformatics</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Rohan">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    
+    <!--- link --->
+<!--
+    <link rel="stylesheet" href="css/font-awesome.min.css">  
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <link rel="canonical" href="#">
+     <!-- end -->
+     
+     <!-- Custom CSS -->
+<!--
+     <link rel="stylesheet" href="style.css/style.css"> 
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+-->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<!--
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-165589332-1"></script>
+</head>
+-->
+-->
+
+<!--
+<body>
+	<div class="wrap-1">
+		<header class="container">
+            <div class="row">
+				<div class="col-3 col-t-5 col-m-6 col-p-9">
+					<h1><a href="Rohan.html">Rohan Ghosh</a></h1>
+				</div>
+				<div>
+                 
+                   
+                    <nav class="col-9 col-t-9">
+                        <ul>
+                            <li><a href="Rohan.html">About</a></li>
+                            <li><a href="Publication.html">Publication</a></li>
+                            <li><a href="Academics.html">Academics</a></li>
+                            <li><a href="RWorkshop.html">Associates</a></li>
+                        </ul>
+                    </nav>
+                    <div class="clear"></div>
+                </div>
+            </div>
+        </div>
+    </header>
+    </div>
+-->
+
+
+<div class="Participation">
+<div class="col-12 col-t-12 col-m-12 ">
+<h2 style="text-align: center;">Scientific Conferences & Workshop</h2>
+<table>                                         
+<tr>
+<td><h3>2021:</h3></td>
+<td><b>International Conference cum Workshop on: Translational Sciences in Drug Discovery and Develoment </b>
+<br>
+<i>Jointly organized by – Indian Science Congress Association (ISCA) & Department of Bioinformatics, Pondicherry Central University, Puduchery, India.</i></td>
+</tr>
+<tr><tr>
+<td><h3>2021:</h3></td>
+<td><b>Training Program on: NGS Data Analysis in Bioinformatics </b>
+<br>
+<i>Organized by DYJ Life Scieneces Pvt. Ltd registered by Indian Ministry of M.S.M.E. </i></td>
+</tr>
+<td><h3>2021:</h3></td>
+<td><b>Science Communication Workshop 2021.</b>
+<br>
+<i>Organized by Indian Institute of Technology (IIT) Bombay, India.</i></td>
+<tr><tr>
+
+</tr>
+</tr>
+</table>
+</div>
+
+</div>
+</div>
+</div>
+
+<!--- Social media --->
+ 
+<div class="wrap-4 col-12 col-m-12 col-p-12">
+	<div class="container5">
+		<div class="row">
+			<div class="social">
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.linkedin.com/in/rohan-ghosh-8a53a0155" target="_blank"><i class="fab fa-linkedin"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.researchgate.net/profile/Rohan-Ghosh-6" target="_blank"><i class="fab fa-researchgate"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://twitter.com/RohanG_26" target="_blank"><i class="fab fa-twitter"></i></a></div>
+				<div class="col-1 col-m-1 col-p-4"><a href="https://www.facebook.com/rohan.ghosh.520900" target="_blank"><i class="fab fa-facebook"></i></a></div>
+            </div>
+        </div>
+	</div>
+</div>
+    
+
+
+
+<!-- social media end -->
+<!--
+</body>
+</html>
+-->
+    
